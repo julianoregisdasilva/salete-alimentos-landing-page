99df57f284cca4231a3702b8d66601b172ab9669
Juliano Regis da Silva <157991737+julianoregisdasilva@users.noreply.github.com>
2026-07-16 22:08:06 -0300

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -59,5 +59,5 @@ export const categories: ProductCategory[] = [
     products: [
       { name: "Vídeo1", url: "videos/video1.mp4"},
       { name: "Vídeo2", url: "videos/video2.mp4"},
-    ]
+    ],
 ];
